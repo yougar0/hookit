9868d6930a9063776b71bccf5aaf80591ff816cd
yougar <3266904708@qq.com>
2018-02-08 00:37:41 +0800

    add call original func

--- test/hook.c
+++ test/hook.c
@@ -8,32 +8,33 @@ typedef unsigned char u8;
 #define PAGESIZE 4096
 #define PAGE_START(page) (~(PAGESIZE - 1) & page)
 
+int (*old_add)(int, int);
+
 int add(int a, int b) {
     return a + b;
 }
 
 int sub(int a, int b) {
     *(&a) = a + 2;
-    return a - b;
+    return old_add(a, b);
 }
 
 int main(int argc, char** argv) {
     csh handle;
     cs_insn * ins;
     size_t count;
-    if(cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
-        return -1;
-    }
     int base = 14;
     int most = 50;
     int size = 0;
     size_t j = 0;
+    if(cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+        return -1;
+    }
 
     u8* addr = add;
     for(int i = base; i < most; i++) {
         count = cs_disasm(handle, addr, i, 0, 0, &ins);
         if(count > 0) {
-            ;
             for(j = 0; j < count; j++) {
                 size += ins[j].size;
             }
@@ -69,6 +70,8 @@ int main(int argc, char** argv) {
     *(unsigned long*)&addr[6] = (unsigned long)trampline;
     mprotect(PAGE_START((unsigned long)add), PAGESIZE, PROT_READ | PROT_EXEC);
     int a = 2, b =2;
+    old_add = &trampline[14];
     printf("%d + %d = %d\n", a, b, add(a, b));
+    munmap(trampline, size + 28);
     return 0;
 }
